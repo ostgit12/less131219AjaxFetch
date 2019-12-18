f2db3fa694a40ac1cb5b715ccc9f95beed59d10b
ostgit12 <zostap@ymail.com>
2019-12-18 12:23:11 +0000

    work

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
 
+let glStr = '';
+
 /*
 const timeElement = document.querySelector('.page-loaded');
 timeElement.innerText = new Date().toLocaleTimeString();
+*/
 
-///////////AJAX-Fetch get HTML sample
+//AJAX-Fetch get HTML sample
+/*
 const btnGetHtmlAjax = document.querySelector('.get-html-ajax');
 btnGetHtmlAjax.addEventListener('click', getHtmlAjax);
 function getHtmlAjax() {
@@ -12,6 +16,7 @@ function getHtmlAjax() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             document.querySelector('.html-container')
                 .innerHTML = xhr.responseText;
+            glStr = xhr.responseText;
         }
     }
     xhr.open('GET', 'client-data.html', true);
@@ -30,8 +35,7 @@ function fetchHtml() {
 */
 
 
-////////JSON Ajax
-
+//Json Ajax
 const btnGetJsonAjax = document.querySelector('.get-json-ajax');
 btnGetJsonAjax.addEventListener('click', getJsonAjax);
 function getJsonAjax() {
@@ -41,19 +45,20 @@ function getJsonAjax() {
             const data = JSON.parse(xhr.responseText);
             document.querySelector('.client-name').innerText = data.name;
             document.querySelector('.balance').innerText = data.balance;
+     
         }
     }
-    xhr.open('GET', 'client-data.json', true);
+    xhr.open('GET', 'logins.json', true);
     xhr.send();
 }
 
 
-///////Json Fetch
+//Json Fetch
 const btnFetchJson = document.querySelector('.fetch-json');
 btnFetchJson.addEventListener('click', fetchJson);
 
 function fetchJson() {
-    fetch('client-data.json')
+    fetch('logins.json')
         .then( response => response.json() )
         .then( data =>  {
             document.querySelector('.client-name').innerText = data.name;
@@ -62,12 +67,12 @@ function fetchJson() {
 }
 
 
-/*Login form Func
+//Login form Func
 document.querySelector('.login-form input[type=submit]')
     .addEventListener('click', login);
 
 function login(e) {
-    e.preventDefault();
+    //e.preventDefault();
     fetch('login', {
         method: 'POST',
         headers: {
@@ -75,82 +80,76 @@ function login(e) {
             // 'Content-Type': 'application/x-www-form-urlencoded',
         },
         body: JSON.stringify({
-            product: document.querySelector('.login-form input[name=name]').value,
-            price: document.querySelector('.login-form input[name=password]').value
+            name: document.querySelector('.login-form input[name=name]').value,
+            //password: document.querySelector('.login-form input[name=password]').value
+            balance: shopList
         })
     })
         .then(_ => document.querySelector('.login-form').reset());
 }
-end Login Func*/
-
-
-document.querySelector('.login-form input[type=submit]')
-    .addEventListener('click', login);
-
-function login(e) {
-    e.preventDefault();
-    fetch('login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: JSON.stringify({
-            product: document.querySelector('.login-form input[name=name]').value,
-            quantity: document.querySelector('.login-form input[name=range]').value,
-            colour: document.querySelector('.login-form select[name=itmColor]').value,
-            sale: document.querySelector('.login-form select[name=itmSale]').value,
-            price: document.querySelector('.login-form input[name=password]').value
-        })
-    })
-        .then(_ => document.querySelector('.login-form').reset());
-}
+//end Login Func
 
 
 /*
-const timeElement = document.querySelector('.page-loaded');
-timeElement.innerText = new Date().toLocaleTimeString();
+document.querySelector('.login-form input[type=submit]')
+    .addEventListener('click', login);
 
-//////////////////////
-/*Shopping List*/
+function login(e) {
+    e.preventDefault();
+    fetch('login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify({
+            //product: document.querySelector('.login-form input[name=name]').value,
+            //quantity: document.querySelector('.login-form input[name=itmRange]').value,
+            //colour: document.querySelector('.login-form select[name=itmColor]').value,
+            //sale: document.querySelector('.login-form select[name=itmSale]').value,
+            //price: document.querySelector('.login-form input[name=password]').value
+            shopList: document.querySelector('.login-form input[name=name]').value+";"
+            +document.querySelector('.login-form input[name=itmRange]').value+";"
+            +document.querySelector('.login-form select[name=itmColor]').value+";"
+            +document.querySelector('.login-form select[name=itmSale]').value+";"
+            +document.querySelector('.login-form input[name=password]').value
+        })
+    })
+        .then(_ => document.querySelector('.login-form').reset());
+}
+*/
+
+/*
+//Shopping List
 const list = document.querySelector('ul');
 const input = document.querySelector('input');
 const button = document.querySelector('button');
-const newItem = document.querySelector('.addItem');
+const newItem = document.querySelector('.addItem');//btn add new item
 
 newItem.addEventListener('click', addNewItem);
 
 function addNewItem() {
-  const itemTxt = document.querySelector('.itemTxt');
-  const rngValue = document.querySelector('.rngBar');
-  const itemColor = document.querySelector('.color');
+  const itemTxt = document.querySelector('.itemName');
+  const itemQuantity = document.querySelector('.itemRange');
+  const itemColor = document.querySelector('.itemColor');
+  const itemSale = document.querySelector('.itemSale');
   const itemPrice = document.querySelector('.itemPrice');
-  const itemDiscount = document.querySelector('.discount'); 
-    
-  let fullPrice = 0;
-    
-    function CalcPrice(itmPrice, itmQuantity, itmDiscount){
-      const itemOff = document.querySelector('.discount');
-    
-    if(itemOff.value === itmDiscount)
-        return itmPrice * itmQuantity;
-    else if(itemDiscount.value === itmDiscount)
-    return ((itmQuantity * itemPrice *50)/100);
-    else if(itemDiscount.value === itmDiscount)
-    return ((itmQuantity * itemPrice *25)/100);
-    else if(itemDiscount.value === itmDiscount)
-    return ((itmQuantity * itemPrice *10)/100);
-    
-}
+  const itemCalcPrice = document.querySelector('.itemCalcPrice');
+ 
       
   let myItem = itemTxt.value
-  +" (quantity: "+rngValue.value
+  +" (quantity: "+itemQuantity.value
   +", colour: "+itemColor.value
-  +", discount: "+itemDiscount.value+"% Off, "
-  +", price: $"+(itemDiscount.value === "0"?itemPrice.value*rngValue.value:((itemPrice.value*rngValue.value*itemDiscount.value)/100))
+  +", discount: "+itemSale.value+"% Off, "
+  +", price: $"+(itemSale.value === "0"?itemPrice.value*itemQuantity.value:((itemPrice.value*itemQuantity.value*itemSale.value)/100))
   +")";
+  
+  itemCalcPrice.value = '';
   itemTxt.value = '';
 
+  itemCalcPrice.value = (itemSale.value === "0"?itemPrice.value*itemQuantity.value:((itemPrice.value*itemQuantity.value*itemSale.value)/100));
+  
+  
 
 const listItem = document.createElement('li');
 const listText = document.createElement('span');
@@ -168,28 +167,118 @@ list.removeChild(listItem);
   input.focus();
   
 }
+*/
 
 
-////////JSON AJAX Shopping list
+const list = document.querySelector('ol');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const newItem = document.querySelector('.btnAddItem');
+
+//array shopList
+const itemsList = document.querySelectorAll('#shopList li');
+let shopList = null;
+shopList = [ ];
+let productCards = null;
+productCards = [];
+
+newItem.addEventListener('click', addNewItem);
+
+function addNewItem() {
+  const itemTxt = document.querySelector('.itemName');
+  const itemQuantity = document.querySelector('.itemRange');
+  const itemColor = document.querySelector('.itemColor');
+  const itemSale = document.querySelector('.itemSale');
+  const itemPrice = document.querySelector('.itemPrice');
+  const itemCalcPrice = document.querySelector('.itemCalcPrice');
+    
+    
+  let myItem = itemTxt.value
+  +" (quantity: "+itemQuantity.value
+  +", colour: "+itemColor.value
+  +", discount: "+itemSale.value+"% Off"
+  +", price: $"+(itemSale.value === "0"?itemPrice.value*itemQuantity.value:((itemPrice.value*itemQuantity.value*itemSale.value)/100))
+  +")";
+
+  
+  //fills array productCards objs
+    let productCard = {};
+    productCard.name = itemTxt.value;
+    productCard.quantity = itemQuantity.value;
+    productCard.color = itemColor.value;
+    productCard.sale = itemSale.value;
+    productCard.price = itemCalcPrice.value;
+
+    productCards.push(productCard);
+
+
+  //glStr = myItem;
+  itemCalcPrice.value = '';
+  itemTxt.value = '';
+    
+itemCalcPrice.value = (itemSale.value === "0"?itemPrice.value*itemQuantity.value:((itemPrice.value*itemQuantity.value*itemSale.value)/100));
+
+
+const listItem = document.createElement('li');
+const listText = document.createElement('span');
+const listBtn = document.createElement('button');
+
+listItem.appendChild(listText);
+listText.textContent = myItem;
+listItem.appendChild(listBtn);
+listBtn.textContent = 'Delete';
+list.appendChild(listItem);
+
+
+//add next OL item to array shopList
+let addItemStr = list.appendChild(listItem).innerHTML;
+alert("added: "+addItemStr);
+shopList.push(addItemStr);
+
+
+listBtn.onclick = function(e) {
+let delItemStr = list.removeChild(listItem).innerHTML;
+
+
+//delete index productCard from array
+for(let k=0; k<productCards.length;k++){
+  if(delItemStr === productCards[k]){
+alert("del cardItem# "+k);
+productCards.pop();
+}
+}
+
+//delete index shopList from array
+for(let k=0; k<shopList.length;k++){
+  if(delItemStr === shopList[k])
+  alert("del listItem# "+k);
+  shopList.pop();
+}
+
+list.removeChild(listItem);
+}
+  input.focus();
+  
+}
+
+
 /*
-const btnGetJsonAjax = document.querySelector('.get-json-ajax');
+//JSON Ajax
+const btnGetJsonAjax = document.querySelector('.shop-json-ajax');
 btnGetJsonAjax.addEventListener('click', getJsonAjax);
 function getJsonAjax() {
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             const data = JSON.parse(xhr.responseText);
-            document.querySelector('.product').innerText = data.product;
-            document.querySelector('.quantity').innerText = data.quantity;
-            document.querySelector('.colour').innerText = data.colour;
-            document.querySelector('.sale').innerText = data.sale;
-            document.querySelector('.price').innerText = data.price;
+            document.querySelector('.product').innerText = data.product;//name;
+            document.querySelector('.quantity').innerText = data.quantity;//balance;
+             document.querySelector('.colour').innerText = data.colour;//balance;
+              document.querySelector('.sale').innerText = data.sale;//balance;
+               document.querySelector('.price').innerText = data.price;//balance;
         }
     }
     xhr.open('GET', 'logins.json', true);
     xhr.send();
 }
-
-////End JSON AJAX Shopping List
-
-
+*/
